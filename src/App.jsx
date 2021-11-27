ce51df074feb8ab2dbfb0e158116cd9fdc1c3833
EdwMore <edwinmoreno903@gmail.com>
2021-11-27 13:38:36 -0500

    Agregado Pasarela de Pago

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,14 @@ import { Row } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 
+import { PasarelaDePago } from "./components/PasarelaPago";
+
 export function App() {
     
 
     return (
-        <div class="container">
-            <div>
-                hola m
-            </div>
-            <button>
-                hey
-            </button>
-            <Table>
-                <Row className="mx-0">
-                    <Button as={Col} variant="primary">Button #1</Button>
-                    <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-                    <Button as={Col} variant="success">Button #3</Button>
-                </Row>
-            </Table>
-        </div>
-
+        <body className="bg-dark vh-100 text-white">
+            <PasarelaDePago />
+        </body>
     ); 
 }
