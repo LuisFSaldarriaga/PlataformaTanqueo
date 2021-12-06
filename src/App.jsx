8e5b1306e1a85e4b3bf5260a96631bebf2089b73
LuisFSaldarriaga <lufesaca@hotmail.com>
2021-12-05 19:06:16 -0500

    add modales registro y login, creación del modal fijo Terms.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export function App() {
     return (
         <div className="bg-dark vh-100 text-white">
             {/*<HeaderUserI></HeaderUserI>*/}
-            <Login/>
+            <Singup/>
             <FooterUserI></FooterUserI>
         </div>
     ); 
